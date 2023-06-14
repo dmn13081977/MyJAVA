e6e6065f3a8cf4d208a2cd8edfde18c2fc8b4cde
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-14 14:14:17 +0300

    Add JAVA

---
+++ MyFirstProject/Task2/Example2.java
@@ -0,0 +1,11 @@
+public class Example2 {
+	public static void main (String[] args) {
+		int num;
+		num = 100;
+		System.out.println("Значение num: " + num);
+		num = num * 2;
+		System.out.print("Значение num * 2: ");
+		System.out.println(num);
+	}
+}
+
