1c3f5103d2b9a9f99b0e6ac5ac4bfc98a13f0847
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-21 15:33:19 +0300

    Revert "delete Project"
    This reverts commit d2e22d4d3523de14c8c50a733cd7cca39320054e.

---
+++ Project/Task2/ifsample.java
@@ -0,0 +1,12 @@
+public class ifsample {
+	public static void main (String[] args) {
+		int x, y;
+		x = 10;
+		y = 20;
+		if (x < y) System.out.println("Значение x меньше y");
+		x = x * 2;
+		if (x == y) System.out.println("Теперь значение х равно у");
+		x = x * 2;
+		if (x > y) System.out.println("Теперь значение х больше у");
+	}
+}
