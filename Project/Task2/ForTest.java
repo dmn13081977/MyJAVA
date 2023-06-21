d2e22d4d3523de14c8c50a733cd7cca39320054e
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-21 14:03:15 +0300

    delete Project

--- Project/Task2/ForTest.java
+++
@@ -1,7 +0,0 @@
-class ForTest {
-	public static void main (String[] args) {
-		int x;
-		for(x = 0; x < 10; x += 1)
-			System.out.println("Значение х: " + x);
-	}
-}
--- Project/Task2/Smile.java
+++
@@ -1,9 +0,0 @@
-class Smile {
-	public static void main (String[] args) {
-		int smile = 0x1F600; // Здесь шестнадцатеричный код эмоджи
-		 StringBuilder sb = new StringBuilder();
-		 sb.append(Character.toChars(smile)); // Собираем в StringBuilder
-		 System.out.println("Улыбающееся лицо: " + sb.toString()); // Выводим
-}
-}
-
