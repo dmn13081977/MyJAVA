581e35232d33e4046ba170988a2c983edf515492
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-23 11:47:51 +0300

    Выведение типов локальных переменных

---
+++ Project/glava3/VarDemo.java
@@ -0,0 +1,11 @@
+class VarDemo {
+	public static void main (String[] args) {
+	       var avg = 10.0;
+       System.out.println("Значение avg: " + avg);
+int var = 1;
+System.out.println("Значение var: " + var);
+var k = -var;
+System.out.println("Значение k: " + k);
+	}
+}
+
