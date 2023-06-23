6fadf40ee8743bf155575030b1fd2dcef1991dd6
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-23 13:36:55 +0300

    Операция деления по модулю

---
+++ Project/glava4/Modulus.java
@@ -0,0 +1,9 @@
+class Modulus {
+	public static void main (String[] args) {
+		var x = 42;
+		double y = 42.25;
+		System.out.println("х по модулю 10 = " + x % 10);
+		System.out.println("у по модулю 10 = " + y % 10);
+	}
+}
+
