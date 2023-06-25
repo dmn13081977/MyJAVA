24dbad796b2449b53f0c7892ee15590904b79c3e
Dmitry Shehovtsov <shehovtzov@mail.ru>
2023-06-25 14:40:31 +0300

    Тернарный оператор

---
+++ Project/glava4/Ternary.java
@@ -0,0 +1,15 @@
+class Ternary {
+	public static void main (String[] args) {
+		int i, k;
+		i = 10;
+		k = i < 0 ? -i : i;
+		System.out.print("Абсолютная величина ");
+		System.out.println(i + " равна " + k);
+
+		i = -10;
+		k = i < 0 ? -i : i;
+		System.out.print("Абсолютная величина ");
+		System.out.println(i + " равна " + k);
+	}
+}
+
